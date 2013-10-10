659046037501e43d4b93ab019604014e026ff64f
Ry Walker <ry@rywalker.com>
2013-10-10 16:27:25 -0400

    Return the text

--- helper.js
+++ helper.js
@@ -1,13 +1,13 @@
 if (typeof Handlebars !== 'undefined') {
   Handlebars.registerHelper("signedInAs", function(date) {
     if (Meteor.user().username) {
-      Meteor.user().username;
+      return Meteor.user().username;
     } else if (Meteor.user().profile.name) {
-      Meteor.user().profile.name;
+      return Meteor.user().profile.name;
     } else if (Meteor.user().emails && Meteor.user().emails[0]) {
-      Meteor.user().emails[0].address;
+      return Meteor.user().emails[0].address;
     } else {
-      "Logged In";
+      return "Signed In";
     }
   });
 }
