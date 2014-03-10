fc994c1afe10dfcbb54b7aae2b0cf9fc5ba1f5b6
Josh Owens <joshua.owens@gmail.com>
2014-03-10 10:55:07 -0400

    Don't bring in spark, we don't need to and it will go away soon.  Also, accounts-t9n handles just-i18n dep now.

--- package.js
+++ package.js
@@ -12,7 +12,6 @@ Package.on_use(function(api) {
     'underscore',
     'templating',
     'handlebars',
-    'spark',
     'session',
     'coffeescript',
     'less']
@@ -58,7 +57,7 @@ Package.on_use(function(api) {
   api.imply('accounts-base', ['client', 'server']);
   api.export('AccountsEntry', ['client', 'server']);
   api.use('iron-router', ['client', 'server']);
-  api.use(['just-i18n', 'accounts-t9n'], ['client']);
+  api.use(['accounts-t9n'], ['client']);
   api.add_files(['shared/router.coffee'], ['client', 'server']);
 
 });
