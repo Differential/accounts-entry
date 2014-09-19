24279dd8612ba659262953df8e58762eeff5af92
alethes <alethesdev@icloud.com>
2014-09-19 10:56:09 +0200

    Remove deprecated api.use() references

--- package.js
+++ package.js
@@ -7,7 +7,6 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom("METEOR@0.9.0");
 
-  api.use(['iron:router', 'mrt:accounts-t9n'], ['client', 'server']);
   // CLIENT
   api.use([
     'deps',
@@ -75,8 +74,7 @@ Package.onUse(function(api) {
   api.imply('accounts-base', ['client', 'server']);
   api.imply('accounts-password', ['client', 'server']);
   api.export('AccountsEntry', ['client', 'server']);
-  api.use('iron-router', ['client', 'server']);
-  api.use(['accounts-t9n'], ['client', 'server']);
+  api.use(['iron:router', 'mrt:accounts-t9n'], ['client', 'server']);
   api.addFiles(['shared/router.coffee'], ['client', 'server']);
 
 });
