7ed65ee6657c407f952c9f854177ba982b1f5a84
Max Nowack <maxnowack@users.noreply.github.com>
2014-11-17 09:05:26 +0100

    Update package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: "Make signin and signout their own pages with routes.",
     version: '1.0.0',
-    name: "oafmedium:accounts-entry",
+    name: "joshowens:accounts-entry",
     githubUrl: 'https://github.com/Differential/accounts-entry',
 });
 
