94a9ea8916de397e4db5e25bc37591bdc4e293c8
dovrosenberg <dov@rosenbergmail.com>
2015-06-30 22:34:31 -0400

    set version on flow router

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom("METEOR@0.9.0");
 
-  api.use(['meteorhacks:flow-router', 'softwarerero:accounts-t9n@1.0.3', 'joshowens:simple-form@0.2.2', 'sacha:spin@2.0.4'], ['client', 'server']);
+  api.use(['meteorhacks:flow-router@1.17.1', 'softwarerero:accounts-t9n@1.0.3', 'joshowens:simple-form@0.2.2', 'sacha:spin@2.0.4'], ['client', 'server']);
   // CLIENT
   api.use([
       'deps',
